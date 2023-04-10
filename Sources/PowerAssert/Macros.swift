1192ef96bebf5f5e5ca69b3dae62108818682fd7
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2023-04-10 12:24:52 +0900

    Rename the Assert function to #expect and leave the original #powerAssert function as an alias for #expect.

--- Sources/PowerAssert/Macros.swift
+++ Sources/PowerAssert/Macros.swift
@@ -1,4 +1,22 @@
-@freestanding(expression) public macro powerAssert(
+/// Asserts that the given expression is true.
+/// - Parameters:
+///   - expression: The expression to evaluate.
+///   - message: An optional description of a failure.
+///   - file: The file where the failure occurs. The default is the filename of the test case where you call this function.
+///   - line: The line number where the failure occurs. The default is the line number where you call this function.
+///   - verbose: If `true`, the result is always printed instead of only when the assertion fails.
+@freestanding(expression)
+public macro expect(
+    _ expression: @autoclosure () throws -> Bool,
+    _ message: @autoclosure () -> String = "",
+    file: StaticString = #filePath,
+    line: UInt = #line,
+    verbose: Bool = false
+) = #externalMacro(module: "PowerAssertPlugin", type: "PowerAssertMacro")
+
+/// An alias for `expect`.
+@freestanding(expression)
+public macro powerAssert(
   _ expression: @autoclosure () throws -> Bool,
   _ message: @autoclosure () -> String = "",
   file: StaticString = #filePath,
