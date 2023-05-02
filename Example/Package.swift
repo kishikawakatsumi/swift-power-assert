5385074ddf01106893c11c579b27f85c5f97854e
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2023-05-03 05:55:08 +0900

    Update example

--- Example/Package.swift
+++ Example/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/kishikawakatsumi/swift-power-assert.git",
-      branch: "0.5.2"
+      branch: "0.6.0"
     ),
   ],
   targets: [
