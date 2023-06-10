1b0f7efa879967a966a6171f01b3b6cf44ec2388
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2023-06-11 08:44:04 +0900

    Update Examples

--- Examples/XcodeProject/MyAppTests/MyAppTests.swift
+++ Examples/XcodeProject/MyAppTests/MyAppTests.swift
@@ -3,14 +3,6 @@ import XCTest
 import PowerAssert
 
 final class MyAppTests: XCTestCase {
-  override func setUpWithError() throws {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-  }
-
-  override func tearDownWithError() throws {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-  }
-
   func testExample() throws {
     let numbers = [1, 2, 3, 4, 5]
 
