f772e523806ce38c2447a7a69c1830bfacb2326b
kishikawa katsumi <kishikawakatsumi@mac.com>
2023-09-11 05:23:41 +0900

    Linux example fix

--- Examples/SwiftPackage/Package.swift
+++ Examples/SwiftPackage/Package.swift
@@ -2,6 +2,7 @@
 
 import PackageDescription
 
+#if !os(Linux)
 let package = Package(
   name: "swift-power-assert-example",
   platforms: [
@@ -23,3 +24,33 @@ let package = Package(
     ),
   ]
 )
+#else
+let package = Package(
+  name: "swift-power-assert-example",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/kishikawakatsumi/swift-power-assert.git",
+      from: "0.12.0"
+    ),
+    .package(
+      url: "https://github.com/apple/swift-syntax.git",
+      from: "509.0.0-swift-DEVELOPMENT-SNAPSHOT-2023-09-05-a"
+    ),
+  ],
+  targets: [
+    .testTarget(
+      name: "ExampleTests",
+      dependencies: [
+        .product(name: "PowerAssert", package: "swift-power-assert"),
+        .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+        .product(name: "SwiftOperators", package: "swift-syntax"),
+        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+      ]
+    ),
+  ]
+)
+#endif
