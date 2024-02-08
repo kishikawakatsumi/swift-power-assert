a25f67cbb6179dfc10a1fa2dbff3ba237df24ec4
GitHub <noreply@github.com>
2024-02-08 19:46:24 +0000

    Update dependencies

--- Playground/backend/deps.ts
+++ Playground/backend/deps.ts
@@ -1,7 +1,7 @@
-export { copy } from "https://deno.land/std@0.214.0/fs/copy.ts";
+export { copy } from "https://deno.land/std@0.215.0/fs/copy.ts";
 export {
   zipReadableStreams,
-} from "https://deno.land/std@0.214.0/streams/mod.ts";
-export { serveFile } from "https://deno.land/std@0.214.0/http/file_server.ts";
+} from "https://deno.land/std@0.215.0/streams/mod.ts";
+export { serveFile } from "https://deno.land/std@0.215.0/http/file_server.ts";
 export { router } from "https://deno.land/x/rutt@0.2.0/mod.ts";
 export { cryptoRandomString } from "https://deno.land/x/crypto_random_string@1.1.0/mod.ts";
